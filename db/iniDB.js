48b32fd4f81ded9b3fc64cfe731b15c098194769
Calixto <calixto.lopez.santos@gmail.com>
2022-06-08 13:04:17 +0200

    Createtable users

--- db/iniDB.js
+++ db/iniDB.js
@@ -12,6 +12,15 @@ async function main() {
     await connection.query(`DROP TABLE IF EXISTS vote_recomendation`);
     await connection.query(`DROP TABLE IF EXISTS recomendation`);
     await connection.query(`DROP TABLE IF EXISTS users`);
+    console.log('create tables');
+    await connection.query(`CREATE TABLE users(
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          name VARCHAR(80),
+                          email VARCHAR(100) UNIQUE NOT NULL,
+                          password VARCHAR(512) NOT NULL
+    )`);
+    await connection.query(`CREATE TABLE reacomendation()`);
+    await connection.query(`CREATE TABLE vote_recomendation()`);
   } catch (error) {
     console.log(error);
   } finally {
