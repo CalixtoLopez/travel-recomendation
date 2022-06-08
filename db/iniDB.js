f6d62c9a1d0af424950c763ec5ac6dbc71b5e715
Calixto <calixto.lopez.santos@gmail.com>
2022-06-08 12:55:38 +0200

    Delete tables

--- db/iniDB.js
+++ db/iniDB.js
@@ -8,6 +8,10 @@ async function main() {
   let connection;
   try {
     connection = await getConnection();
+    console.log('delete tables');
+    await connection.query(`DROP TABLE IF EXISTS vote_recomendation`);
+    await connection.query(`DROP TABLE IF EXISTS recomendation`);
+    await connection.query(`DROP TABLE IF EXISTS users`);
   } catch (error) {
     console.log(error);
   } finally {
